23f39bebeb4ba4f2d9a5cef40b3822788db491ef
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-11-30 15:33:00 -0300

    Update full_otus.mk

--- full_otus.mk
+++ full_otus.mk
@@ -21,10 +21,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from otus device
-$(call inherit-product, device/motorola/otus/device.mk)
+$(call inherit-product, device/microsoft/fh/device.mk)
 
-PRODUCT_NAME := full_otus
-PRODUCT_DEVICE := otus
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := otus
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_NAME := full_fh
+PRODUCT_DEVICE := fh
+PRODUCT_BRAND := microsoft
+PRODUCT_MODEL := fh
+PRODUCT_MANUFACTURER := microsoft
